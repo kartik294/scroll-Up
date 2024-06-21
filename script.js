7c58bb88c1747f2285effde47fdf204be8a6af8b
kartik294 <kartikgeu@gmail.com>
2024-06-21 08:31:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let lastScrollTop = 0;
+let navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", function() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    navbar.classList.add('active');
+  } else {
+    navbar.classList.remove('active');
+  }
+  lastScrollTop = scrollTop;
+});
